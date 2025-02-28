553b136d6a751a15d745a459c3eb4cfd3d2f757d
Fardin Sheikh Tiham <tihamsheikhone@gmail.com>
2025-02-28 18:09:36 +0600

    Add files via upload

---
+++ tkinter calculatro v3/calculator.py
@@ -0,0 +1,149 @@
+# A simple calculator made with tkinter
+# it has all the basic calculation functions
+# also there are additional scientific functions like sin, cos, factorial etc
+# (Radioactive theme)
+
+
+# used packages
+import tkinter as tk
+import math
+
+# main window
+app = tk.Tk()
+app.title("Premium Calculator")
+
+# apps background color
+app.config(bg="black")
+
+# fixed window size
+app.resizable(False, False)
+
+# operation function
+def click(btn):
+    if (inp_dis.get() == "Error " or inp_dis.get() == "infinite ") and btn != "AC":   # error time input blocking (error handling)
+        return
+
+    if inp_dis.get() == "" and btn == "=":  # empty submit ( error handling)
+        inp_dis.set("")
+
+    elif btn == "=":  # submission
+        try:
+            if "M" in inp_dis.get():    # remainder
+                print(inp_dis.get())
+                temp = inp_dis.get().replace("M", "%")
+                inp_dis.set(eval(temp))
+
+            elif inp_dis.get().count("^") > 2:
+                inp_dis.set("Error ")
+
+            try:
+                expression = inp_dis.get().replace("^", "**")   # exponential
+                res = expression.replace("%", "/100 *")         # percentage
+                inp_dis.set(eval(res))                                      # evaluation of an equation
+            except ZeroDivisionError:
+                inp_dis.set("infinite ")
+
+        except Exception:
+            inp_dis.set("Error ")   # error handling
+
+    elif btn == "π":    # value for pi
+        try:
+            inp_dis.set(inp_dis.get() + str(math.pi))
+
+        except Exception:
+            inp_dis.set("Error ")   # error handling
+
+    elif btn == "Exp":  # value for eulars const
+        try:
+            inp_dis.set(inp_dis.get() + str(math.e))
+        except Exception:
+            inp_dis.set("Error ")   # error handling
+
+    elif btn == "log":  # equation for log10
+        try:
+            res = math.log10(float(inp_dis.get()))
+            inp_dis.set(str(res))
+
+        except Exception:
+            inp_dis.set("Error ")   # error handling
+
+    elif btn == "x!":   # factorial of a number
+        try:
+            expression = inp_dis.get().replace("x!", "")
+            res = math.factorial(int(expression))
+            inp_dis.set(str(res))
+
+        except Exception:
+            inp_dis.set("Error ")   # error handling
+
+    elif btn == "sin":  # sin, cos, tan function
+        try:
+            res = math.sin(math.radians(float(inp_dis.get())))
+            inp_dis.set((str(res)))
+
+        except Exception:
+            inp_dis.set("Error ")   # error handling
+
+    elif btn == "cos":
+        try:
+            res = math.cos(math.radians(float(inp_dis.get())))
+            inp_dis.set(str(res))
+        except Exception:
+            inp_dis.set("Error ")   # error handling
+
+    elif btn == "tan":
+        try:
+            res = math.tan(math.radians(float(inp_dis.get())))
+            inp_dis.set(str(res))
+        except Exception:
+            inp_dis.set("Error ")   # error handling
+
+    elif btn == "√":    # square root
+        try:
+            res = math.sqrt(float(inp_dis.get()))
+            inp_dis.set(str(res))
+        except Exception:
+            inp_dis.set("Error")    # error handling
+
+    elif btn == "del":  # single delete
+        word = inp_dis.get()
+        inp_dis.set(word[:len(word)-1])
+
+    elif btn == "AC":   # all delete
+        inp_dis.set("")
+
+    else:   # value adding
+        inp_dis.set(inp_dis.get() + btn)
+
+
+# main variable
+inp_dis = tk.StringVar()
+
+# input display and design
+vis_dis = tk.Entry(master=app, textvariable=inp_dis, bd=8, font="Arial 22", bg="lime", justify="right")
+vis_dis.grid(row=0, column=0, columnspan=4, ipadx=23, ipady=18, padx=3, pady=4)
+
+# button list
+button = [
+    ("π", "Exp", "log", "x!"),
+    ("sin", "cos", "tan", "del"),
+    ("√", "^", "%", "/"),
+    ("1", "2", "3", "*"),
+    ("4", "5", "6", "-"),
+    ("7", "8", "9", "+"),
+    (".", "0", "AC", "M"),
+    ("="),
+]
+
+# button placement
+for i, row in enumerate(button):
+    for j, btn_text in enumerate(row):
+        btn = tk.Button(master=app, text=btn_text, font="Arial 20 bold", bd=6, bg="green", command= lambda text=btn_text: click(text))
+        # button desgin
+        if btn_text == "=":
+            btn.grid(row=i + 1, column=j, columnspan=4, ipadx=8, ipady=6, padx=3, pady=3, sticky="nsew")
+        else:
+            btn.grid(row=i + 1, column=j, columnspan=1, ipadx=8, ipady=6, padx=3, pady=3, sticky="nsew")
+
+# runner
+app.mainloop()
